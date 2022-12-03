f2f2e8be3ee44c3e30663f5c88eadb4fe5db2f29
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-03 14:16:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import axios from 'axios'; 
+import refs from './js/refs';
 
-const btnSubmit = document.querySelector('.search__form');
-const input = document.querySelector('.input');
-const filmoteka = document.querySelector('.filmoteka');
-const massage = document.querySelector('.massage')
-
-massage.classList.add('clear');
-
-btnSubmit.addEventListener('submit', onClickBtnSearch);
-
+refs.hideMessage.classList.add('clear');
+refs.searchForm.addEventListener('submit', onClickBtnSearch);
 
 fetchFilmotekaPopularFilms();
 
-async function fetchFilmotekaPopularFilms(){    
+async function fetchFilmotekaPopularFilms(){  
+  refs.hideMessage.classList.add('clear'); 
   try {
     const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=dbc34002be87151e0df6d0e75806eaf7`);
-    const filme = response.data.results; 
-    console.log(filme)    
-    filmoteka.insertAdjacentHTML("beforeend", filme.map(item=>
+    const filme = response.data.results;        
+    refs.filmCards.insertAdjacentHTML("beforeend", filme.map(item=>
       `<div class="photo-card">
         <a class="photo-card__link"  href="https://image.tmdb.org/t/p/w500${item.backdrop_path}">
           <img  src="https://image.tmdb.org/t/p/w500${item.poster_path}" data-source="${item.poster_path}" alt="${item.original_title}" loading="lazy" width="100%" height="90%" style="border-radius: 5px;"/>
@@ -30,18 +24,19 @@ async function fetchFilmotekaPopularFilms(){
     .join(''));
   }
   catch (error) {   
-    massage.classList.remove('clear');    
+    refs.hideMessage.classList.remove('clear');    
   }
 }
 
 function onClickBtnSearch(evt) {
   evt.preventDefault();
-  if(input.value===''){   
+  if(refs.input.value===''){  
+    fetchFilmotekaPopularFilms();
     return;
   }
-  filmoteka.innerHTML ='';
-  massage.classList.add('clear');
-  fetchFilmoteka(input.value.trim()); 
+  refs.filmCards.innerHTML ='';
+  refs.hideMessage.classList.add('clear');
+  fetchFilmoteka(refs.input.value.trim()); 
  
 } 
    
@@ -50,11 +45,11 @@ async function fetchFilmoteka(name){
     const response = await axios.get(`https://api.themoviedb.org/3//search/movie?api_key=dbc34002be87151e0df6d0e75806eaf7&query=${name}`);
     const filme = response.data.results;         
     if (filme.length === 0){ 
-      massage.classList.remove('clear');    
-      filmoteka.innerHTML ='';      
+      refs.hideMessage.classList.remove('clear');    
+      refs.filmCards.innerHTML = '';      
     }     
     else {               
-      filmoteka.insertAdjacentHTML("beforeend", filme.map(item=>
+      refs.filmCards.insertAdjacentHTML("beforeend", filme.map(item=>
         `<div class="photo-card">
         <a class="photo-card__link"  href="https://image.tmdb.org/t/p/w500${item.backdrop_path}">
           <img  src="https://image.tmdb.org/t/p/w500${item.poster_path}" data-source="${item.poster_path}" alt="${item.original_title}" loading="lazy" width="100%" height="90%" style="border-radius: 5px;"/>
@@ -68,7 +63,7 @@ async function fetchFilmoteka(name){
     }       
   }
   catch (error) {   
-    massage.classList.remove('clear'); 
+    refs.hideMessage.classList.remove('clear'); 
   }
 }
 
