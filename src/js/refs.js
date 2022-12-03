99ecc89c5c10bdaaddcf3cede6e48343312adef3
DmitryNikolin <ni-ko-dim@ukr.net>
2022-12-03 11:14:28 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,31 @@
+export const refs = {
+  body: document.querySelector('body'),
+  input: document.getElementById('input'),
+  searchForm: document.getElementById('search-form'),
+  filmCards: document.querySelector('.js-film-cards'),
+
+  searchBtn: document.querySelector('.js-search-btn'),
+
+  header: document.getElementById('header'),
+  homeLink: document.querySelector('.js-link-home'),
+  headerInput: document.querySelector('.js-input-wrapper'),
+  libraryLink: document.querySelector('.js-link-library'),
+  headerBtn: document.querySelector('.js-button-wrapper'),
+  logoLink: document.querySelector('.js-logo-link'),
+  headerQueueBtn: document.querySelector('.js-header-queue-btn'),
+  headerWatchedBtn: document.querySelector('.js-header-watched-btn'),
+
+  backdrop: document.querySelector('.js-modal-overlay'),
+  modal: document.querySelector('.js-modal'),
+  closeBtn: document.querySelector('.js-close-modal'),
+  main: document.querySelector('.wrapper'),
+
+  homePageContainer: document.querySelector('.js-home-page-container'),
+  libraryPageContainer: document.querySelector('.js-library-container'),
+  libraryFilmCards: document.querySelector('.js-library-film-cards'),
+
+  scrollBtn: document.querySelector('.js-scroll-btn'),
+  preloadWrap: document.querySelector('.preload'),
+
+  paginationContainer: document.querySelector('.js-pagination'),
+};
