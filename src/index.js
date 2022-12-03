d4b2b7b3c303bf45f172b691e97b1a2d47cdd31c
nirqqen97 <93587073+nirqqen97@users.noreply.github.com>
2022-12-03 17:57:37 +0200

    FIx?
     я вроде все перелапатил и должно все работать проверь пожалуйста

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import axios from 'axios'; 
+import axios from 'axios';
+import {refs} from './js/refs'; 
 
-const btnSubmit = document.querySelector('.search__form');
-const input = document.querySelector('.input');
-const filmoteka = document.querySelector('.filmoteka');
-const massage = document.querySelector('.massage')
-
-massage.classList.add('clear');
-
-btnSubmit.addEventListener('submit', onClickBtnSearch);
-
+refs.hideText.classList.add('clear');
+refs.searchForm.addEventListener('submit', onClickBtnSearch);
 
 fetchFilmotekaPopularFilms();
 
@@ -17,7 +11,7 @@ async function fetchFilmotekaPopularFilms(){
     const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=dbc34002be87151e0df6d0e75806eaf7`);
     const filme = response.data.results; 
     console.log(filme)    
-    filmoteka.insertAdjacentHTML("beforeend", filme.map(item=>
+    refs.markUp.insertAdjacentHTML("beforeend", filme.map(item=>
       `<div class="photo-card">
         <a class="photo-card__link"  href="https://image.tmdb.org/t/p/w500${item.backdrop_path}">
           <img  src="https://image.tmdb.org/t/p/w500${item.poster_path}" data-source="${item.poster_path}" alt="${item.original_title}" loading="lazy" width="100%" height="90%" style="border-radius: 5px;"/>
@@ -30,18 +24,18 @@ async function fetchFilmotekaPopularFilms(){
     .join(''));
   }
   catch (error) {   
-    massage.classList.remove('clear');    
+    refs.hideText.classList.remove('clear');    
   }
 }
 
 function onClickBtnSearch(evt) {
   evt.preventDefault();
-  if(input.value===''){   
+  if(refs.input.value===''){   
     return;
   }
-  filmoteka.innerHTML ='';
-  massage.classList.add('clear');
-  fetchFilmoteka(input.value.trim()); 
+  refs.markUp.innerHTML ='';
+  refs.hideText.classList.add('clear');
+  fetchFilmoteka(refs.input.value.trim()); 
  
 } 
    
@@ -50,11 +44,11 @@ async function fetchFilmoteka(name){
     const response = await axios.get(`https://api.themoviedb.org/3//search/movie?api_key=dbc34002be87151e0df6d0e75806eaf7&query=${name}`);
     const filme = response.data.results;         
     if (filme.length === 0){ 
-      massage.classList.remove('clear');    
-      filmoteka.innerHTML ='';      
+      refs.hideText.classList.remove('clear');    
+      refs.markUp.innerHTML ='';      
     }     
     else {               
-      filmoteka.insertAdjacentHTML("beforeend", filme.map(item=>
+      refs.markUp.insertAdjacentHTML("beforeend", filme.map(item=>
         `<div class="photo-card">
         <a class="photo-card__link"  href="https://image.tmdb.org/t/p/w500${item.backdrop_path}">
           <img  src="https://image.tmdb.org/t/p/w500${item.poster_path}" data-source="${item.poster_path}" alt="${item.original_title}" loading="lazy" width="100%" height="90%" style="border-radius: 5px;"/>
@@ -68,7 +62,7 @@ async function fetchFilmoteka(name){
     }       
   }
   catch (error) {   
-    massage.classList.remove('clear'); 
+    refs.hideText.classList.remove('clear'); 
   }
 }
 
